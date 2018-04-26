bc1e45e784d1efb196ce1ef3819f1222d73e90f7
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-04-26 12:03:58 -0700

    adding questions to html

--- server.js
+++ server.js
@@ -9,21 +9,13 @@ var app = express();
 // setting up port
 var PORT = process.env.PORT || 8080;
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
-});
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.write("you posted:\n");
-  res.end(JSON.stringify(req.body, null, 2));
-});
+require("./app/routing/htmlRoutes.js")(app);
 
 // listening to port
 app.listen(PORT, function() {
