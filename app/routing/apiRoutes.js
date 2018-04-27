f12155ffaa164c9a6aef0917d363badafb44d8f2
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-04-27 11:16:45 -0700

    clean up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     res.json(data);
   });
 
-  app.post("/api/new", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     var newFriend = {
       name: req.body.name,
       photo: req.body.photo,
