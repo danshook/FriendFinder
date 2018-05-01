3e0e4eb7ab5e3182c41cc8a15c149f0d547409ac
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-05-01 14:47:57 -0700

    added beginning of readme. Added more logic and math to best match.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,11 +5,37 @@ module.exports = function(app) {
     res.json(data);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/new", function(req, res) {
     var newFriend = {
       name: req.body.name,
       photo: req.body.photo,
       scores: req.body.scores
     };
-  });
-};
+    var userScores = newFriend.scores;
+    var userData = req.body;
+    var userScores = userData.scores;
+    console.log("USER SCORES : " + userScores);
+
+    //console.log("SCORES: " + userScores);
+    //console.log("REQ: " + JSON.stringify(req));
+
+    for (var i = 0; i < data.length; i++) {
+      var diff = 0;
+      for (j = 0; j < data[i].scores[j]; j++) {
+        var currentFriendScore = data[i].scores[j];
+      };
+        var diff = Math.abs(
+         // math for user scores!
+        );
+      }
+      // assign least diiference to bestMatch.
+      if (diff <= bestMatch.pointDifference) {
+       // final comparison
+      }
+      console.log('BEST MATCH: ' + JSON.stringify(bestMatch));
+    }
+    //console.log("NEW FREIND: " + JSON.stringify(newFriend));
+    // res.end();
+
+
+    data.push(userData);
