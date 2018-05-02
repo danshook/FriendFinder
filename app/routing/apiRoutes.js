6892a3b904003bde4c49c13685b56a49949ca9e0
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-05-01 18:40:44 -0700

    data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,27 +15,26 @@ module.exports = function(app) {
     var userData = req.body;
     var userScores = userData.scores;
     console.log("USER SCORES : " + userScores);
+    console.log("NEW FRIEND : " + JSON.stringify(newFriend));
+  });
+};
+//console.log("SCORES: " + userScores);
+//console.log("REQ: " + JSON.stringify(req));
 
-    //console.log("SCORES: " + userScores);
-    //console.log("REQ: " + JSON.stringify(req));
+// for (var i = 0; i < data.length; i++) {
+//   var diff = 0;
+//   for (j = 0; j < data[i].scores[j]; j++) {
+//     var currentFriendScore = data[i].scores[j];
+//   };
+//     var diff = Math.abs(
+// math for user scores!
+//   );
+// }
+// assign least diiference to bestMatch.
+// if (diff <= bestMatch.pointDifference) {
+// final comparison
+// }
+// console.log('BEST MATCH: ' + JSON.stringify(bestMatch));
 
-    for (var i = 0; i < data.length; i++) {
-      var diff = 0;
-      for (j = 0; j < data[i].scores[j]; j++) {
-        var currentFriendScore = data[i].scores[j];
-      };
-        var diff = Math.abs(
-         // math for user scores!
-        );
-      }
-      // assign least diiference to bestMatch.
-      if (diff <= bestMatch.pointDifference) {
-       // final comparison
-      }
-      console.log('BEST MATCH: ' + JSON.stringify(bestMatch));
-    }
-    //console.log("NEW FREIND: " + JSON.stringify(newFriend));
-    // res.end();
-
-
-    data.push(userData);
+//console.log("NEW FREIND: " + JSON.stringify(newFriend));
+// res.end();
